1368843d95c9e2fd3870fa8b053c124199b2c025
rperhoc <111128583+rperhoc@users.noreply.github.com>
2023-03-03 10:27:56 +0100

    Izpis v eni vrsti, podatek o valuti se pridobi iz API arraya.

--- console.php
+++ console.php
@@ -3,5 +3,4 @@
 $btc_json = file_get_contents('https://api.coinbase.com/v2/prices/BTC-USD/spot');
 $btc_array = json_decode($btc_json, true);
 
-echo "Currency: " . $btc_array['data']['base'] . "\n";
-echo "USD value: " . $btc_array['data']['amount'] . "\n";
+echo $btc_array['data']['base'] . ": {$btc_array['data']['currency']} " . $btc_array['data']['amount'];
