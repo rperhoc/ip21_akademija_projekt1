fe7197880585c6642fe0f50c77326113441d9a5a
rperhoc <111128583+rperhoc@users.noreply.github.com>
2023-03-03 10:17:11 +0100

    Dokončana naloga 2

--- console.php
+++ console.php
@@ -1,3 +1,7 @@
 <?php
 
-echo "Hello World!";
+$btc_json = file_get_contents('https://api.coinbase.com/v2/prices/BTC-USD/spot');
+$btc_array = json_decode($btc_json, true);
+
+echo "Currency: " . $btc_array['data']['base'] . "\n";
+echo "USD value: " . $btc_array['data']['amount'] . "\n";
