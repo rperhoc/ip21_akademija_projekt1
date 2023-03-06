69b2a5c3c770c864f39a99fc9db74f33bdfd087f
rperhoc <111128583+rperhoc@users.noreply.github.com>
2023-03-06 19:00:40 +0100

    Dokončana naloga 3.

--- console.php
+++ console.php
@@ -1,6 +1,22 @@
 <?php
 
-$btc_json = file_get_contents('https://api.coinbase.com/v2/prices/BTC-USD/spot');
-$btc_array = json_decode($btc_json, true);
+<?php
 
-echo $btc_array['data']['base'] . ": {$btc_array['data']['currency']} " . $btc_array['data']['amount'];
+if ($argc == 1 or strtolower($argv[1]) == 'help') {
+    echo "Help Text";
+    exit();
+} elseif ($argc == 3) {
+    $crypto = $argv[1];
+    $fiat = $argv[2];
+} else {
+    echo "Invalid number of arguments.";
+    exit();
+}
+
+$api_url = "https://api.coinbase.com/v2/prices/{$crypto}-{$fiat}/spot";
+$data_json = file_get_contents($api_url);
+$data_array = json_decode($data_json, true);
+
+echo "{$data_array['data']['base']} = {$data_array['data']['amount']} {$data_array['data']['currency']}\n";
+
+?>
